e8535e85e441b99d67eb0c7b47382b417470efd3
Addy897 <145797684+Addy897@users.noreply.github.com>
2024-06-12 17:36:44 +0530

    added model down indicator

--- src/routes/query/+server.js
+++ src/routes/query/+server.js
@@ -6,22 +6,26 @@ import { AI_API } from '$env/static/private';
 export async function POST({request}) {
     let {inputC,image_link}=await request.json()
     const data={user_input:inputC,image_url:image_link}
-	try{
-    const response = await fetch(AI_API,
-		{
-      
-			headers: {'Content-Type': 'application/json' },
-			method: "POST",
-			body: JSON.stringify(data),
-		}
-	);
-}catch{
-	return new Response("AI Model is down. Please try again later.")
-}
 	let result;
-	result = await response.json();
-    result=result.response
-	
+	try{
+		const response = await fetch(AI_API,
+			{
+		
+				headers: {'Content-Type': 'application/json' },
+				method: "POST",
+				body: JSON.stringify(data),
+			}
+		);
+		try{
+		result = await response.json();
+		result=result.response
+		}catch{
+			result="Please Upload Image and ask question"
+		}
+	}catch{
+		result="AI Model is currently down. Please try again later"
+
+	}
 	
     return new Response(result);
 }
